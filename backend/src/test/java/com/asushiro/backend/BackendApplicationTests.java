7735882e34e4991e5b4ebf239cdb19d69e134e75
asushiro <hka397056@163.com>
2024-01-21 15:45:22 +0800

    增加后端框架

---
+++ backend/src/test/java/com/asushiro/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asushiro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
